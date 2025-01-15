984c62b77c901db3691b7278c656210632875074
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2025-01-15 16:15:00 +0530

    Update

--- SchoolRegistration.Data/Data/ApplicationDbContext.cs
+++ SchoolRegistration.Data/Data/ApplicationDbContext.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolRegistration.Entities.Models; // Ensure the namespace is included
 
-namespace SchoolRegistration.Data.Data;
-
-public partial class ApplicationDbContext : DbContext
+namespace SchoolRegistration.Data.Data
 {
-    public ApplicationDbContext()
+    public partial class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet for SchoolBasicInformation
+        public DbSet<SchoolBasicInformation> SchoolBasicInformation { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-OKN4EUA\\SQLEXPRESS; Database=StudentRegistration_Db; Integrated Security=True; TrustServerCertificate=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Ensure base implementation is called
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-OKN4EUA\\SQLEXPRESS; Database=StudentRegistration_Db; Integrated Security=True; TrustServerCertificate=True;");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ SchoolRegistration.Entities/Model/SchoolBasicInformation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SchoolRegistration.Entities.Model
+{
+    public class SchoolBasicInformation
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Invalid School UDISE Code")]
+        [RegularExpression("^[0-9]{11}$", ErrorMessage = "The UDISE Code must be exactly 11 digits.")]
+        public string? UDISECode { get; set; }
+
+        [Unicode(false)]
+        [StringLength(50)]
+        public string? SchoolNameEnglish { get; set; }
+
+        [Unicode(true)]
+        [StringLength(50)]
+        public string? SchoolNameHindi { get; set; }
+
+
+        [Range(1980, 2025, ErrorMessage = "Year must be between 1980 and the current year")]
+        public int YearOfEstablishment  { get; set; }
+
+        public DateOnly Recognition {  get; set; }
+
+        [Unicode(false)]
+        [StringLength(30)]
+        public string? SchoolBoard { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
