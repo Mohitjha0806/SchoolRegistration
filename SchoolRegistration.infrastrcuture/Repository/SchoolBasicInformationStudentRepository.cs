c768a74c13eabcf384fe4f96f3d36371cacc056e
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2025-01-18 17:05:08 +0530

    Update

---
+++ SchoolRegistration.infrastrcuture/Repository/SchoolBasicInformationStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegistration.infrastrcuture.Repository
+{
+    internal class SchoolBasicInformationStudentRepository
+    {
+    }
+}
